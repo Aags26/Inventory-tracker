1bf974fa7bb594c7c8c465ed9bf3d42a218a40fd
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-10-25 11:04:50 +0530

    Fragment for user essentials under dashboard

--- app/src/main/java/com/bphc/oops_project/fragments/dashboard/EssentialsFragment.java
+++ app/src/main/java/com/bphc/oops_project/fragments/dashboard/EssentialsFragment.java
@@ -1,4 +1,22 @@
 package com.bphc.oops_project.fragments.dashboard;
 
-public class EssentialsFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.bphc.oops_project.R;
+
+public class EssentialsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_essentials, container, false);
+        return view;
+    }
 }
