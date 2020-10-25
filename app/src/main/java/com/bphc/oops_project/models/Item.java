17f36d05d2b462c0b81d6643055d6cecd36433c6
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-10-26 00:50:07 +0530

    Model class for item

--- app/src/main/java/com/bphc/oops_project/models/Item.java
+++ app/src/main/java/com/bphc/oops_project/models/Item.java
@@ -1,4 +1,38 @@
 package com.bphc.oops_project.models;
 
 public class Item {
+
+    public int image;
+    public String name;
+    public String quantity;
+
+    public Item(int image, String name, String quantity) {
+        this.image = image;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
 }
