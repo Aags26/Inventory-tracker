696ac7d4326ab344e99fb4217f5caa6c1692d6f9
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-10-26 00:50:25 +0530

    Model class for an item group

--- app/src/main/java/com/bphc/oops_project/models/ItemGroup.java
+++ app/src/main/java/com/bphc/oops_project/models/ItemGroup.java
@@ -1,4 +1,31 @@
 package com.bphc.oops_project.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemGroup {
+
+    public String group;
+    public ArrayList<Item> items;
+
+    public ItemGroup(String group, ArrayList<Item> items) {
+        this.group = group;
+        this.items = items;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
 }
