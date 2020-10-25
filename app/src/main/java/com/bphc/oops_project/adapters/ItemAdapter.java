46d3e31a121711b33a1d897a838952cb9eb6f9e0
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-10-26 00:47:24 +0530

    Adapter for each item of an item group

--- app/src/main/java/com/bphc/oops_project/adapters/ItemAdapter.java
+++ app/src/main/java/com/bphc/oops_project/adapters/ItemAdapter.java
@@ -1,4 +1,60 @@
 package com.bphc.oops_project.adapters;
 
-public class ItemAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bphc.oops_project.R;
+import com.bphc.oops_project.models.Item;
+
+import java.util.ArrayList;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+
+    private ArrayList<Item> items;
+
+    public ItemAdapter(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView itemImage;
+        TextView itemName, itemQuantity;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            itemImage = itemView.findViewById(R.id.item_image);
+            itemName = itemView.findViewById(R.id.item_name);
+            itemQuantity = itemView.findViewById(R.id.item_quantity);
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.model_item, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        Item item = items.get(position);
+        holder.itemImage.setImageResource(R.mipmap.ic_launcher);
+        holder.itemName.setText(item.getName());
+        holder.itemQuantity.setText(item.getQuantity());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
 }
