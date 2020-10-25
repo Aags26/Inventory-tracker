500b5a9477604e95059a2b7e9cd702edbab74b84
Aags26 <f20191320@hyderabad.bits-pilani.ac.in>
2020-10-26 00:54:37 +0530

    Fragment for maintaining essentials and to-dos

--- app/src/main/java/com/bphc/oops_project/fragments/DashboardFragment.java
+++ app/src/main/java/com/bphc/oops_project/fragments/DashboardFragment.java
@@ -2,6 +2,7 @@ package com.bphc.oops_project.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,13 +11,51 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bphc.oops_project.R;
+import com.bphc.oops_project.fragments.dashboard.EssentialsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loadFragment(new EssentialsFragment());
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        BottomNavigationView bottomNav = view.findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(this);
+
         return view;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment selectedFragment = null;
+
+        switch (item.getItemId()) {
+
+            case R.id.essentials:
+            case R.id.todo_list:
+                selectedFragment = new EssentialsFragment();
+                break;
+        }
+
+        loadFragment(selectedFragment);
+        return true;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        if (getActivity() != null) {
+            getActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
